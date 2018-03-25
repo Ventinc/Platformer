dbf15b9d75a729986319522119faaf544befd522
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-25 22:54:08 +0200

    Added Spritesheet class

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,14 @@
+import Spritesheet from './Spritesheet.js'
+import {loadImage} from './loaders.js';
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext("2d");
 
 context.fillRect(0, 0, 100, 100);
+
+loadImage('assets/spritesheet/ground.png').then(image => {
+    const sprites = new Spritesheet(image, 128, 128);
+    sprites.define('ground', 0, 1);
+    sprites.draw('ground', context, 45, 62);
+
+})
