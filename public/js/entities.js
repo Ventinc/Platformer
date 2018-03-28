5a5b7fd0ef316ed0abf826881350a84441e89f97
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-28 22:27:14 +0200

    Added createMario for entities

---
+++ public/js/entities.js
@@ -0,0 +1,21 @@
+import Entity from './Entity.js';
+import {loadMarioSprite} from './assets.js';
+
+
+export function createMario() {
+    return loadMarioSprite()
+            .then(sprite => { 
+                const mario = new Entity();
+                
+                mario.draw = function drawMario(context) {
+                    sprite.draw('idle', context, this.pos.x, this.pos.y);
+                }
+
+                mario.update = function updateMario(deltaTime) {
+                    this.pos.x += this.vel.x * deltaTime;
+                    this.pos.y += this.vel.y * deltaTime;
+                }
+
+                return mario;
+            });
+}
