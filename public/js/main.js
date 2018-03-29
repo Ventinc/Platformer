8be135f300a28f281466ebbf46f68d6a571089de
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-30 00:27:54 +0200

    Added Keyboard to handle jump of mario

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,8 @@ import {createMario} from './entities.js';
 import {loadBackgroundSprites} from './assets.js';
 import {createBackgroundLayer, createSpriteLayer} from './layers.js';
 
+import Keyboard from './KeyboardState.js'
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext("2d");
 
@@ -22,9 +24,20 @@ Promise.all([
     const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
     comp.layers.push(backgroundLayer);
 
-    const gravity = 30;
+    const gravity = 2000;
     mario.pos.set(64, 180);
-    mario.vel.set(200, -600);
+    
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+        console.log(keyState);
+        if (keyState) {
+            mario.jump.start();
+        } else {
+            mario.jump.cancel();
+        }
+    })
+    input.listenTo(window);
 
 
     const spriteLayer = createSpriteLayer(mario);
@@ -33,9 +46,9 @@ Promise.all([
     const timer = new Timer();
 
     timer.update = function update(time) {
-        comp.draw(context);
         mario.update(time);
-        mario.vel.y += gravity;
+        comp.draw(context);
+        mario.vel.y += gravity * time;
     }
 
     timer.start();
