a29e3ee56aae34032a417173a5944b1246c7e3c4
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-30 00:27:32 +0200

    Change Mario to work with Velocity and Jump Trait

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,19 +1,18 @@
-import Entity from './Entity.js';
+import Entity, {Trait} from './Entity.js';
+import Velocity from './traits/Velocity.js';
+import Jump from './traits/Jump.js'
 import {loadMarioSprite} from './assets.js';
-
 
 export function createMario() {
     return loadMarioSprite()
             .then(sprite => { 
                 const mario = new Entity();
                 
+                mario.addTrait(new Velocity());
+                mario.addTrait(new Jump());
+
                 mario.draw = function drawMario(context) {
                     sprite.draw('idle', context, this.pos.x, this.pos.y);
-                }
-
-                mario.update = function updateMario(deltaTime) {
-                    this.pos.x += this.vel.x * deltaTime;
-                    this.pos.y += this.vel.y * deltaTime;
                 }
 
                 return mario;
