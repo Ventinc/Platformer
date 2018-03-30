1fbbfb66709677fd9c465eae511b4d95b7abb229
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-30 19:05:17 +0200

    Added TileCollider class

---
+++ public/js/TileCollider.js
@@ -0,0 +1,36 @@
+import TileResolver from './TileResolver.js'
+
+export default class TileCollider {
+    constructor(tileMatrix) {
+        this.tiles = new TileResolver(tileMatrix);
+    }
+
+    checkY(entity) {
+        const matches = this.tiles.searchByRange(
+                                                    entity.pos.x,
+                                                    entity.pos.x + entity.size.x,
+                                                    entity.pos.y,
+                                                    entity.pos.y + entity.size.y);
+    
+        matches.forEach(match => {
+
+            if (match.tile.name !== 'ground') return;
+            
+            if (entity.vel.y > 0) {
+                if (entity.pos.y + entity.size.y > match.y1) {
+                    entity.pos.y = match.y1 - entity.size.y;
+                    entity.vel.y = 0;
+                }
+            } else if (entity.vel.y < 0) {
+                if (entity.pos.y < match.y2) {
+                    entity.pos.y = match.y2;
+                    entity.vel.y = 0;
+                }
+            }
+        })
+    }
+
+    test(entity) {
+        this.checkY(entity);
+    }
+}
