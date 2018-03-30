ec8e4202b113892cf32852c7a26b3e3886ca7b73
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-30 18:12:48 +0200

    Added forEach method for Matrix class

--- public/js/Math.js
+++ public/js/Math.js
@@ -3,6 +3,14 @@ export class Matrix {
         this.grid = new Array();
     }
 
+    forEach(callback) {
+        this.grid.forEach((column, x) => {
+            column.forEach((value, y) => {
+                callback(value, x, y);            
+            });
+        });    
+    }
+
     set(x, y, value) {
         if (!this.grid[x]) {
             this.grid[x] = new Array();
