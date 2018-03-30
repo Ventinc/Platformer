50d995fb16ee2e3de96e2c638a8716bf9cccca7d
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-30 18:13:24 +0200

    Change createBackgroundLayer to work with level and matrix

--- public/js/layers.js
+++ public/js/layers.js
@@ -1,25 +1,17 @@
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    })
-}
-
-export function createBackgroundLayer(backgrounds, sprites) {
+export function createBackgroundLayer(level, sprites) {
     const buffer = document.createElement('canvas');
     buffer.width = 256;
     buffer.height = 240;
-
-    backgrounds.forEach(background => {
-        drawBackground(background, buffer.getContext("2d"), sprites);
+    
+    const context = buffer.getContext("2d");
+    
+    level.tiles.forEach((tile, x, y) => {
+        sprites.drawTile(tile.name, context, x, y);
     });
 
     return function drawBackgroundLayer(context) {
         context.drawImage(buffer, 0, 0);
-    }
+    };
 }
 
 export function createSpriteLayer(entities) {
