27ff91ab3974d330a16ad40e319131e82559cb7e
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-30 17:56:47 +0200

    Added Matrix class in Math file

--- public/js/Math.js
+++ public/js/Math.js
@@ -1,3 +1,25 @@
+export class Matrix {
+    constructor() {
+        this.grid = new Array();
+    }
+
+    set(x, y, value) {
+        if (!this.grid[x]) {
+            this.grid[x] = new Array();
+        }
+        this.grid[x][y] = value;
+    }
+
+    get(x, y) {
+        const col = this.grid[x];
+
+        if (col) {
+            return col[y];
+        }
+        return undefined;
+    }
+}
+
 export class Vec2 {
     constructor(x, y) {
         this.set(x, y);
