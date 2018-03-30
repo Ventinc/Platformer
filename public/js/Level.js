69686b6a4334cf40de70de703d1c51581424f817
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-30 19:05:30 +0200

    Added tileCollider in Level class

--- public/js/Level.js
+++ public/js/Level.js
@@ -1,13 +1,19 @@
+import TileCollider from './TileCollider.js';
 import Compositor from './Compositor.js';
-import {Matrix} from './Math.js'
+import {Matrix} from './Math.js';
 export default class Level {
     constructor() {
         this.comp = new Compositor();
         this.entities = new Set();
         this.tiles = new Matrix();
+
+        this.tileCollider = new TileCollider(this.tiles);
     }
 
     update(deltaTime) {
-        this.entities.forEach(entity => entity.update(deltaTime));
+        this.entities.forEach(entity => {
+            entity.update(deltaTime);   
+            this.tileCollider.test(entity);
+        });
     }
 }
