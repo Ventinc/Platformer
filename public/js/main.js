fd9386ec522194abe2112c3724a0052f7ba75838
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-31 19:51:28 +0200

    Added camera to game

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
+import Camera from './Camera.js';
 import Timer from './Timer.js';
-import {createCollisionLayer} from './layers.js';
+import {createCollisionLayer, createCameraLayer} from './layers.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
 import {setupKeyboard} from './input.js'
+import {setupMouseControl} from './debug.js'
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext("2d");
@@ -15,17 +17,27 @@ Promise.all([
     mario,
     level
 ]) => {
+    const camera = new Camera();
+    window.camera = camera;
+
     mario.pos.set(64, 180);
     
+    level.comp.layers.push(
+        createCollisionLayer(level),
+        createCameraLayer(camera));
+       
+
     level.entities.add(mario);
     
     const input = setupKeyboard(mario);
     input.listenTo(window);
 
+    setupMouseControl(canvas, mario, camera);
+
     const timer = new Timer();
     timer.update = function update(time) {
         level.update(time);
-        level.comp.draw(context);
+        level.comp.draw(context, camera);
     }
 
     timer.start();
