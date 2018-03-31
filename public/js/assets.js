af299d996f888272d4631e798ebb06070d324c2e
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-31 19:49:58 +0200

    remove loadBackgroundSprites

--- public/js/assets.js
+++ public/js/assets.js
@@ -1,16 +1,6 @@
 import Spritesheet from './Spritesheet.js';
 import {loadImage} from './loaders.js';
 
-export function loadBackgroundSprites() {
-    return loadImage('assets/spritesheet/ground.png')
-        .then(image => {
-            const sprites = new Spritesheet(image, 16, 16);
-            sprites.defineTile('ground', 0, 0);
-            sprites.defineTile('sky', 3, 23);
-            return sprites;
-        });
-}
-
 export function loadMarioSprite() {
     return loadImage('assets/tileset/characters.gif')
     .then(image => {
