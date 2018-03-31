699150e1a923ffc93cffed9b965fae3290bce6d0
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-03-31 16:58:27 +0200

    Epur main function to be more readable

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,7 @@ import Timer from './Timer.js';
 import {createCollisionLayer} from './layers.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
-
-import Keyboard from './KeyboardState.js'
+import {setupKeyboard} from './input.js'
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext("2d");
@@ -16,43 +15,17 @@ Promise.all([
     mario,
     level
 ]) => {
-
-    const gravity = 2000;
     mario.pos.set(64, 180);
     
     level.entities.add(mario);
     
-    const SPACE = 32;
-    const input = new Keyboard();
-    input.addMapping(SPACE, keyState => {
-        if (keyState) {
-            mario.jump.start();
-        } else {
-            mario.jump.cancel();
-        }
-    });
-    input.addMapping(39, keyState => {
-        mario.go.direction = keyState
-    })
-    input.addMapping(37, keyState => {
-        mario.go.direction = -keyState
-    })
+    const input = setupKeyboard(mario);
     input.listenTo(window);
-
-    ['mousedown', 'mousemove'].forEach(eventName => {
-        canvas.addEventListener(eventName, event => {
-            if (event.buttons === 1) {
-                mario.vel.set(0, 0);
-                mario.pos.set(event.offsetX, event.offsetY);
-            }
-        })
-    });
 
     const timer = new Timer();
     timer.update = function update(time) {
         level.update(time);
         level.comp.draw(context);
-        mario.vel.y += gravity * time;
     }
 
     timer.start();
