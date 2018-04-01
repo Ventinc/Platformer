c0ef123c44467e306c48332c79fd472f7a9b9300
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-01 20:14:28 +0200

    Change loadLevel import

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 import Camera from './Camera.js';
 import Timer from './Timer.js';
 import {createCollisionLayer, createCameraLayer} from './layers.js';
-import {loadLevel} from './loaders.js';
+import {loadLevel} from './loaders/level.js';
 import {createMario} from './entities.js';
 import {setupKeyboard} from './input.js'
 
