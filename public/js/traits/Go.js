c604afebf3a1d5482a8990629cab0cb256efb0c7
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-01 18:21:11 +0200

    Update Go and Jump to be more representative of mario first game

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -5,17 +5,36 @@ export default class Go extends Trait {
         super('go')
 
         this.direction = 0;
-        this.speed = 6000;
+        this.acceleration = 400;
+        this.deceleration = 300;
+        this.dragFactor = 1 / 5000;
         this.distance = 0;
         this.heading = 1;
     }
 
     update(entity, deltaTime) {
-        entity.vel.x = this.speed * this.direction * deltaTime;
+        const absX = Math.abs(entity.vel.x)
+        
         if (this.direction) {
-            this.heading = this.direction;
-            this.distance += Math.abs(entity.vel.x * deltaTime);
-        } else
+            entity.vel.x += this.acceleration * deltaTime * this.direction;
+            if (entity.jump) {
+                if (entity.jump.falling === false) {
+                    this.heading = this.direction;
+                }
+            } else {
+                this.heading = this.direction;                
+            }
+        } else if (entity.vel.x !== 0) {
+            const decel = Math.min(absX, this.deceleration * deltaTime);
+            entity.vel.x += entity.vel.x > 0 ? -decel : decel;
+        } else {
             this.distance = 0;
+        }
+        
+        const drag = this.dragFactor * entity.vel.x * absX;
+        entity.vel.x -= drag;
+
+        this.distance += absX * deltaTime;
     }
+
 }
--- public/js/traits/Jump.js
+++ public/js/traits/Jump.js
@@ -1,26 +1,53 @@
-import {Trait} from '../Entity.js'
-
+import {Trait, Sides} from '../Entity.js'
 export default class Jump extends Trait {
     constructor() {
         super('jump')
 
-        this.duration = 0.5;
+        this.ready = 0;
+        this.duration = 0.3;
         this.velocity = 200;
+        this.requestTime = 0;
+        this.gracePeriod = 0.5;
+        this.speedBoost = 0.3;
+
         this.engageTime = 0;
     }
 
     start() {
-        this.engageTime = this.duration;
+        this.requestTime = this.gracePeriod;
     }
 
     cancel() {
         this.engageTime = 0;
+        this.requestTime = 0;        
     }
 
     update(entity, deltaTime) {
+        if (this.requestTime > 0) {
+            if (this.ready > 0) {
+                this.engageTime = this.duration;
+                this.requestTime = 0;
+            }
+            
+            this.requestTime -= deltaTime;
+        }
         if (this.engageTime > 0) {
-            entity.vel.y = -this.velocity;
+            entity.vel.y = -(this.velocity + Math.abs(entity.vel.x) * this.speedBoost);
             this.engageTime -= deltaTime;
+        }
+
+        this.ready--;
+    }
+
+    get falling() {
+        return this.ready < 0;
+    }
+
+    obstruct(entity, side) {
+        if (side === Sides.BOTTOM) {
+            this.ready = 1;
+        } else if (side === Sides.TOP) {
+            this.cancel();
         }
     }
 }
