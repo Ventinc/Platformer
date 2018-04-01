5e5fbd445e0399f5783b95d41df24b30a3169b40
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-01 20:14:06 +0200

    Move loaders functions into level.js in loaders folder

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -3,38 +3,6 @@ import Spritesheet from './Spritesheet.js'
 import { createAnim } from './anim.js'
 import {createBackgroundLayer, createSpriteLayer, createCollisionLayer} from './layers.js';
 
-function createTiles(level, backgrounds) {
-
-    function applyRange(background, xStart, xLen, yStart, yLen) {
-        const xEnd = xStart + xLen;
-        const yEnd = yStart + yLen;
-
-        for (let x = xStart; x < xEnd; ++x) {
-            for (let y = yStart; y < yEnd; ++y) {
-                level.tiles.set(x, y, {
-                    name: background.tile,
-                    type: background.type
-                })
-            }
-        }
-    }
-
-    backgrounds.forEach(background => {
-        background.ranges.forEach(range => {
-            if (range.length === 4) {
-                const [xStart, xLen, yStart, yLen] = range;
-                applyRange(background, xStart, xLen, yStart, yLen);
-            } else if (range.length === 3) {
-                const [xStart, xLen, yStart] = range;
-                applyRange(background, xStart, xLen, yStart, 1);
-            } else if (range.length === 2) {
-                const [xStart, yStart] = range;
-                applyRange(background, xStart, 1, yStart, 1);
-            }
-        })
-    })
-}
-
 export function loadImage(url) {
     return new Promise(resolve => {
         const image = new Image();
@@ -45,7 +13,7 @@ export function loadImage(url) {
     });
 }
 
-function loadJSON(url) {
+export function loadJSON(url) {
     return fetch(url).then(r => r.json());
 }
 
@@ -85,24 +53,4 @@ export function loadSpriteSheet(name) {
 
             return sprites;
         })
-}
-
-export function loadLevel(name) {
-    return loadJSON(`levels/${name}.json`)
-    .then(levelSpec => Promise.all([
-        levelSpec,
-        loadSpriteSheet(levelSpec.spriteSheet)
-    ])).then(([levelSpec, backgroundSprites]) => {
-        const level = new Level();
-
-        createTiles(level, levelSpec.backgrounds);
-
-        const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
-        level.comp.layers.push(backgroundLayer);
-        
-        const spriteLayer = createSpriteLayer(level.entities);
-        level.comp.layers.push(spriteLayer);
-
-        return level;
-    });
 }
