9fe4dfa4440d15ab8a89b1c80f011839971a69be
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-01 17:07:58 +0200

    update mario entity to run animation

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,11 +1,11 @@
 import Entity, {Trait} from './Entity.js';
-import Velocity from './traits/Velocity.js';
 import Jump from './traits/Jump.js';
 import Go from './traits/Go.js';
-import {loadMarioSprite} from './assets.js';
+import { loadSpriteSheet } from './loaders.js';
+import {createAnim} from './anim.js'
 
 export function createMario() {
-    return loadMarioSprite()
+    return loadSpriteSheet('mario')
             .then(sprite => { 
                 const mario = new Entity();
                 mario.size.set(14, 16);
@@ -13,8 +13,17 @@ export function createMario() {
                 mario.addTrait(new Jump());
                 mario.addTrait(new Go());
 
+                const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 10)
+
+                function routeFrame(mario) {
+                    if (mario.go.direction !== 0) {
+                        return runAnim(mario.go.distance);
+                    }
+                    return 'idle'
+                }
+
                 mario.draw = function drawMario(context) {
-                    sprite.draw('idle', context, 0, 0);
+                    sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
                 }
 
                 return mario;
