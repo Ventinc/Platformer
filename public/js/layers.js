4ff3b8f31e67ac8d1c42002b4180ebbfabc77e88
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-01 20:13:42 +0200

    Added clearRect in background layers and remove old optimisations

--- public/js/layers.js
+++ public/js/layers.js
@@ -1,21 +1,16 @@
-export function createBackgroundLayer(level, sprites) {
-    const tiles = level.tiles;
-    const resolver = level.tileCollider.tiles;
+import TileResolver from './TileResolver.js'
+
+export function createBackgroundLayer(level, tiles, sprites) {
+    const resolver = new TileResolver(tiles);
     const buffer = document.createElement('canvas');
     buffer.width = 256 + 16;
     buffer.height = 240;
     
     const context = buffer.getContext("2d");
     
-    let startIndex, endIndex;
-    function redraw(drawFrom, drawTo) {
-
-        // if (drawFrom === startIndex && drawTo === endIndex)
-        //     return;
-
-        startIndex = drawFrom;
-        endIndex = drawTo;
-
+    function redraw(startIndex, endIndex) {
+        context.clearRect(0, 0, buffer.width, buffer.height);
+        
         for(let x = startIndex; x <= endIndex; ++x){
             const col = tiles.grid[x];
             if (col) {
