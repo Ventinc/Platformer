aa162d7dca180c24c49ee353935d844277bd4333
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-01 18:21:32 +0200

    Added turbo function and update frames

--- public/js/entities.js
+++ public/js/entities.js
@@ -4,6 +4,9 @@ import Go from './traits/Go.js';
 import { loadSpriteSheet } from './loaders.js';
 import {createAnim} from './anim.js'
 
+const SLOW_DRAG = 1/1000;
+const FAST_DRAG = 1/5000;
+
 export function createMario() {
     return loadSpriteSheet('mario')
             .then(sprite => { 
@@ -12,11 +15,22 @@ export function createMario() {
 
                 mario.addTrait(new Jump());
                 mario.addTrait(new Go());
+                mario.go.dragFactor = SLOW_DRAG;
 
-                const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 10)
+                mario.turbo = function setTurboState(turboOn) {
+                    this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
+                }
+
+                const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 6)
 
                 function routeFrame(mario) {
-                    if (mario.go.direction !== 0) {
+                    if (mario.jump.falling) {
+                        return 'jump';
+                    }
+                    if (mario.go.distance > 0) {
+                        if ((mario.vel.x > 0 && mario.go.direction < 0) || (mario.vel.x < 0 && mario.go.direction > 0)) {
+                            return 'break'
+                        }
                         return runAnim(mario.go.distance);
                     }
                     return 'idle'
