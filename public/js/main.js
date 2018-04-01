f6ef7d85adddba59c46739ede26ccf4020b6d4b7
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-01 18:21:52 +0200

    Remove unuse and added camera follow player

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,6 @@ import {createCollisionLayer, createCameraLayer} from './layers.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
 import {setupKeyboard} from './input.js'
-import {setupMouseControl} from './debug.js'
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext("2d");
@@ -20,23 +19,21 @@ Promise.all([
     const camera = new Camera();
     window.camera = camera;
 
-    mario.pos.set(64, 180);
-    
-    // level.comp.layers.push(
-    //     createCollisionLayer(level),
-    //     createCameraLayer(camera));
-       
+    mario.pos.set(64, 180);       
 
     level.entities.add(mario);
     
     const input = setupKeyboard(mario);
     input.listenTo(window);
 
-    setupMouseControl(canvas, mario, camera);
+    //setupMouseControl(canvas, mario, camera);
 
     const timer = new Timer();
     timer.update = function update(time) {
         level.update(time);
+        if (mario.pos.x > 100) {
+            camera.pos.x = mario.pos.x - 100;
+        }
         level.comp.draw(context, camera);
     }
 
