169ff7f2ec74ea321b8a65209c9c86efe3056b95
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-02 01:37:53 +0200

    Update TileCollider with bounding

--- public/js/TileCollider.js
+++ public/js/TileCollider.js
@@ -6,43 +6,18 @@ export default class TileCollider {
     }
 
     checkY(entity) {
-        const matches = this.tiles.searchByRange(
-                                                    entity.pos.x,
-                                                    entity.pos.x + entity.size.x,
-                                                    entity.pos.y,
-                                                    entity.pos.y + entity.size.y);
-    
-        matches.forEach(match => {
-
-            if (match.tile.name !== 'ground') return;
-            
-            if (entity.vel.y > 0) {
-                if (entity.pos.y + entity.size.y > match.y1) {
-                    entity.pos.y = match.y1 - entity.size.y;
-                    entity.vel.y = 0;
-                }
-            } else if (entity.vel.y < 0) {
-                if (entity.pos.y < match.y2) {
-                    entity.pos.y = match.y2;
-                    entity.vel.y = 0;
-                }
-            }
-        })
-    }
-
-    checkY(entity) {
         let y;
         if (entity.vel.y > 0) {
-            y = entity.pos.y + entity.size.y;
+            y = entity.bounds.bottom;
         } else if (entity.vel.y < 0) {
-            y = entity.pos.y;
+            y = entity.bounds.top;
         } else {
             return;
         }
 
         const matches = this.tiles.searchByRange(
-                            entity.pos.x,
-                            entity.pos.x + entity.size.x,
+                            entity.bounds.left,
+                            entity.bounds.right,
                             y, y
                         );
     
@@ -51,15 +26,15 @@ export default class TileCollider {
             if (match.tile.type !== 'ground') return;
             
             if (entity.vel.y > 0) {
-                if (entity.pos.y + entity.size.y > match.y1) {
-                    entity.pos.y = match.y1 - entity.size.y;
+                if (entity.bounds.bottom > match.y1) {
+                    entity.bounds.bottom = match.y1;
                     entity.vel.y = 0;
 
                     entity.obstruct(Sides.BOTTOM);
                 }
             } else if (entity.vel.y < 0) {
-                if (entity.pos.y < match.y2) {
-                    entity.pos.y = match.y2;
+                if (entity.bounds.top < match.y2) {
+                    entity.bounds.top = match.y2;
                     entity.vel.y = 0;
 
                     entity.obstruct(Sides.TOP);                    
@@ -71,31 +46,35 @@ export default class TileCollider {
     checkX(entity) {
         let x;
         if (entity.vel.x > 0) {
-            x = entity.pos.x + entity.size.x;
+            x = entity.bounds.right;
         } else if (entity.vel.x < 0) {
-            x = entity.pos.x;
+            x = entity.bounds.left;
         } else {
             return;
         }
 
         const matches = this.tiles.searchByRange(
                             x, x,
-                            entity.pos.y,
-                            entity.pos.y + entity.size.y
+                            entity.bounds.top,
+                            entity.bounds.bottom
                         );
     
         matches.forEach(match => {
             if (match.tile.type !== 'ground') return;
             
             if (entity.vel.x > 0) {
-                if (entity.pos.x + entity.size.x > match.x1) {
-                    entity.pos.x = match.x1 - entity.size.x;
+                if (entity.bounds.right > match.x1) {
+                    entity.bounds.right = match.x1;
                     entity.vel.x = 0;
+
+                    entity.obstruct(Sides.RIGHT);
                 }
             } else if (entity.vel.x < 0) {
-                if (entity.pos.x < match.x2) {
-                    entity.pos.x = match.x2;
+                if (entity.bounds.left < match.x2) {
+                    entity.bounds.left = match.x2;
                     entity.vel.x = 0;
+
+                    entity.obstruct(Sides.LEFT);                    
                 }
             }
         })
