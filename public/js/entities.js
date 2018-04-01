4da34045ad50d36dfd42861ecdca70596d470cb4
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-02 01:38:41 +0200

    create factory loaders for entities

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,45 +1,17 @@
-import Entity, {Trait} from './Entity.js';
-import Jump from './traits/Jump.js';
-import Go from './traits/Go.js';
-import { loadSpriteSheet } from './loaders.js';
-import { createAnim } from './anim.js'
+import {loadKoopa} from './entities/Koopa.js';
+import {loadMario} from './entities/Mario.js';
+import {loadGoomba} from './entities/Goomba.js';
 
-const SLOW_DRAG = 1/1000;
-const FAST_DRAG = 1/5000;
+export function loadEntities() {
+    const entityFactories = {}
+    
+    function addAs(name) {
+        return factory => entityFactories[name] = factory;
+    }
 
-export function createMario() {
-    return loadSpriteSheet('mario')
-            .then(sprite => { 
-                const mario = new Entity();
-                mario.size.set(14, 16);
-
-                mario.addTrait(new Jump());
-                mario.addTrait(new Go());
-                mario.go.dragFactor = SLOW_DRAG;
-
-                mario.turbo = function setTurboState(turboOn) {
-                    this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
-                }
-
-                const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 6)
-
-                function routeFrame(mario) {
-                    if (mario.jump.falling) {
-                        return 'jump';
-                    }
-                    if (mario.go.distance > 0) {
-                        if ((mario.vel.x > 0 && mario.go.direction < 0) || (mario.vel.x < 0 && mario.go.direction > 0)) {
-                            return 'break'
-                        }
-                        return runAnim(mario.go.distance);
-                    }
-                    return 'idle'
-                }
-
-                mario.draw = function drawMario(context) {
-                    sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
-                }
-
-                return mario;
-            });
+    return Promise.all([
+        loadMario().then(addAs('mario')),
+        loadGoomba().then(addAs('goomba')),
+        loadKoopa().then(addAs('koopa'))
+    ]).then(() => entityFactories)
 }
