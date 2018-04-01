40eda29e460d68d9286d31dc63d7e7c3490fb4d2
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-01 17:09:10 +0200

    Added heading and distance for animations

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -6,9 +6,16 @@ export default class Go extends Trait {
 
         this.direction = 0;
         this.speed = 6000;
+        this.distance = 0;
+        this.heading = 1;
     }
 
     update(entity, deltaTime) {
         entity.vel.x = this.speed * this.direction * deltaTime;
+        if (this.direction) {
+            this.heading = this.direction;
+            this.distance += Math.abs(entity.vel.x * deltaTime);
+        } else
+            this.distance = 0;
     }
 }
