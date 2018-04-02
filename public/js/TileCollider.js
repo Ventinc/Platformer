15266da5c74eb6c263dd5b885ac97dafc40e21d1
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-02 23:28:18 +0200

    Update TileCollider to use it in Physics Trait

--- public/js/TileCollider.js
+++ public/js/TileCollider.js
@@ -27,17 +27,11 @@ export default class TileCollider {
             
             if (entity.vel.y > 0) {
                 if (entity.bounds.bottom > match.y1) {
-                    entity.bounds.bottom = match.y1;
-                    entity.vel.y = 0;
-
-                    entity.obstruct(Sides.BOTTOM);
+                    entity.obstruct(Sides.BOTTOM, match);
                 }
             } else if (entity.vel.y < 0) {
                 if (entity.bounds.top < match.y2) {
-                    entity.bounds.top = match.y2;
-                    entity.vel.y = 0;
-
-                    entity.obstruct(Sides.TOP);                    
+                    entity.obstruct(Sides.TOP, match);
                 }
             }
         })
@@ -64,17 +58,11 @@ export default class TileCollider {
             
             if (entity.vel.x > 0) {
                 if (entity.bounds.right > match.x1) {
-                    entity.bounds.right = match.x1;
-                    entity.vel.x = 0;
-
-                    entity.obstruct(Sides.RIGHT);
+                    entity.obstruct(Sides.RIGHT, match);
                 }
             } else if (entity.vel.x < 0) {
                 if (entity.bounds.left < match.x2) {
-                    entity.bounds.left = match.x2;
-                    entity.vel.x = 0;
-
-                    entity.obstruct(Sides.LEFT);                    
+                    entity.obstruct(Sides.LEFT, match);
                 }
             }
         })
