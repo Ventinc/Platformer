cc7c17a816ae86843c04fc9a6e11542318cc98bb
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-02 22:50:32 +0200

    Added Killable Trait

---
+++ public/js/traits/Killable.js
@@ -0,0 +1,29 @@
+import {Trait, Sides} from '../Entity.js'
+
+export default class Killable extends Trait {
+    constructor() {
+        super('killable');
+
+        this.dead = false;
+        this.deadTime = 0;
+        this.removeAfter = 2;
+    }
+
+    kill() {
+        this.dead = true;
+    }
+
+    revive() {
+        this.dead = false;
+        this.deadTime = 0;
+    }
+
+    update(entity, deltaTime, level) {
+        if (this.dead) {
+            this.deadTime += deltaTime;
+            if (this.deadTime > this.removeAfter) {
+                level.entities.delete(entity);
+            }
+        }
+    }
+}
