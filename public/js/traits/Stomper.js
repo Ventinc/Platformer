f0c485d0b5a9d51fd2b0a10a8919f17eef4b4298
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-02 22:50:44 +0200

    Added Stomper Trait

---
+++ public/js/traits/Stomper.js
@@ -0,0 +1,19 @@
+import {Trait, Sides} from '../Entity.js'
+
+export default class Stomper extends Trait {
+    constructor() {
+        super('stomper');
+        this.bounceSpeed = 400;
+    }
+
+    bounce(us, them) {
+        us.bounds.bottom = them.bounds.top;
+        us.vel.y = -this.bounceSpeed;
+    }
+
+    collides(us, them) {
+        if (them.killable && us.vel.y > them.vel.y) {
+            this.bounce(us, them);
+        }
+    }
+}
