bf27d3f101699db5714762fea9d9028b75e3b86c
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-02 22:52:29 +0200

    Added Behavior Trait to Goomba

--- public/js/entities/Goomba.js
+++ public/js/entities/Goomba.js
@@ -1,17 +1,47 @@
-import Entity, {Sides} from '../Entity.js'
+import Entity, {Sides, Trait} from '../Entity.js'
 import {loadSpriteSheet} from '../loaders.js'
-import PendulumWalk from '../traits/PendulumWalk.js';
+import PendulumMove from '../traits/PendulumMove.js';
+import Killable from '../traits/Killable.js';
 
 export function loadGoomba() {
     return loadSpriteSheet('goomba')
     .then(createGoombaFactory);
 }
 
+class Behavior extends Trait {
+    constructor() {
+        super('behavior');
+    }
+
+    collides(us, them) {
+        if (us.killable.dead) {
+            return;
+        }
+        if (them.stomper) {
+            if (them.vel.y > us.vel.y) {
+                us.killable.kill();
+                us.pendulumMove.speed = 0;
+            } else {
+                them.killable.kill();
+            }
+        }
+    }
+}
+
 function createGoombaFactory(sprite) {
 
     const walkAnim = sprite.animations.get('walk');
+
+    function routeAnim(goomba) {
+        if (goomba.killable.dead) {
+            return 'flat';
+        }
+
+        return walkAnim(goomba.lifetime);
+    }
+
     function drawGooma(context) {
-        sprite.draw(walkAnim(this.lifetime), context, 0, 0);
+        sprite.draw(routeAnim(this), context, 0, 0);
     }
 
     return function createGoomba() {
@@ -19,7 +49,9 @@ function createGoombaFactory(sprite) {
         goomba.size.set(16, 16);
         goomba.vel.x = -30;
 
-        goomba.addTrait(new PendulumWalk());
+        goomba.addTrait(new PendulumMove());
+        goomba.addTrait(new Behavior());
+        goomba.addTrait(new Killable());
 
         goomba.draw = drawGooma;
 
