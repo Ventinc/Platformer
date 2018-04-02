824f9c44a3c4bbf8172c148b02faace024fc4de5
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-02 22:49:06 +0200

    Create EntityCollider class

---
+++ public/js/EntityCollider.js
@@ -0,0 +1,19 @@
+
+export default class EntityCollider {
+    constructor(entities) {
+        this.entities = entities
+    }
+
+    check(subject) {
+        this.entities.forEach(candidate => {
+            if (subject === candidate) {
+                return;
+            }
+
+            if (subject.bounds.overlaps(candidate.bounds)) {
+                subject.collides(candidate);
+                candidate.collides(subject);
+            }
+        });
+    }
+}
