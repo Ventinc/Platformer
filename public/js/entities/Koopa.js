0ac33634b5b4f4ff5531be50b91993f4ae580c1d
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-02 22:52:19 +0200

    Added Behavior Trait to Koopa

--- public/js/entities/Koopa.js
+++ public/js/entities/Koopa.js
@@ -1,5 +1,6 @@
-import Entity, {Sides} from '../Entity.js'
-import PendulumWalk from '../traits/PendulumWalk.js'
+import Entity, {Sides, Trait} from '../Entity.js'
+import PendulumMove from '../traits/PendulumMove.js'
+import Killable from '../traits/Killable.js'
 import {loadSpriteSheet} from '../loaders.js'
 
 export function loadKoopa() {
@@ -7,11 +8,112 @@ export function loadKoopa() {
     .then(createKoopaFactory);
 }
 
+const STATE_WALKING = Symbol('walking');
+const STATE_HIDING = Symbol('hiding');
+const STATE_PANIC = Symbol('panic');
+class Behavior extends Trait {
+    constructor() {
+        super('behavior');
+        this.state = STATE_WALKING;
+        this.hideTime = 0;
+        this.hideDuration = 5;
+
+        this.walkSpeed = null;
+        this.panicSpeed = 300;
+    }
+
+    collides(us, them) {
+        if (us.killable.dead) {
+            return;
+        }
+        if (them.stomper) {
+            if (them.vel.y > us.vel.y) {
+                this.handleStomp(us, them);
+            } else {
+                this.handleNudge(us, them);
+            }
+        }
+    }
+
+    handleNudge(us, them) {
+        if (this.state === STATE_WALKING) {
+            them.killable.kill();
+        } else if (this.state === STATE_HIDING) {
+            this.panic(us, them);
+        } else if (this.state === STATE_PANIC) {
+            const travelDir = Math.sign(us.vel.x);
+            const impactDir = Math.sign(us.pos.x - them.pos.x);
+            if (travelDir !== 0 && travelDir !== impactDir) {
+                them.killable.kill();
+            }
+        }
+    }
+
+    handleStomp(us, them) {
+        if (this.state === STATE_WALKING) {
+            this.hide(us);
+        } else if (this.state === STATE_HIDING) {
+            us.killable.kill();
+            us.vel.set(100, -200);
+            us.canCollide = false;
+        } else if (this.state === STATE_PANIC) {
+            this.hide(us);
+        }
+    }
+
+    hide(us) {
+        us.vel.x = 0;
+        us.pendulumMove.enabled = false;
+        if (this.walkSpeed === null) {
+            this.walkSpeed = us.pendulumMove.speed;
+        }
+        this.hideTime = 0;
+        this.state = STATE_HIDING;
+    }
+
+    unhide(us) {
+        us.pendulumMove.enabled = true;
+        us.pendulumMove.speed = this.walkSpeed;
+        this.state = STATE_WALKING;
+    }
+
+    panic(us, them) {
+        us.pendulumMove.enabled = true;
+        us.pendulumMove.speed = this.panicSpeed * Math.sign(them.vel.x);
+        this.state = STATE_PANIC;
+    }
+
+    update(us, deltaTime) {
+        if (this.state === STATE_HIDING) {
+            this.hideTime += deltaTime;
+            if (this.hideTime > this.hideDuration) {
+                this.unhide(us);
+            }
+        }
+    } 
+}
+
 function createKoopaFactory(sprite) {
 
     const walkAnim = sprite.animations.get('walk');
+    const wakeAnim = sprite.animations.get('wake');
+    
+    function routeAnim(koopa) {
+        if (koopa.behavior.state === STATE_HIDING) {
+            if (koopa.behavior.hideTime > 3) {
+                return wakeAnim(koopa.behavior.hideTime)
+            }
+            return 'hiding'
+        }
+
+        if (koopa.behavior.state === STATE_PANIC) {
+            return 'hiding';
+        }
+        return walkAnim(koopa.lifetime);
+    }
+    
     function drawGooma(context) {
-        sprite.draw(walkAnim(this.lifetime), context, 0, 0, this.vel.x < 0);
+        sprite.draw(routeAnim(this), context, 0, 0, this.vel.x < 0);
     }
 
     return function createKoopa() {
@@ -20,7 +122,9 @@ function createKoopaFactory(sprite) {
         koopa.offset.y = 8;
         koopa.vel.x = -30;
 
-        koopa.addTrait(new PendulumWalk());
+        koopa.addTrait(new PendulumMove());
+        koopa.addTrait(new Killable());
+        koopa.addTrait(new Behavior());
 
         koopa.draw = drawGooma;
 
