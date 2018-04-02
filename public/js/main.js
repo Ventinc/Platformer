cac17aa26559a8a82c6d1bff9c751a09508123b5
Vincent Dusautoir <vincent.dusautoir@epitech.eu>
2018-04-02 22:52:46 +0200

    Refactor main.js to use async and await

--- public/js/main.js
+++ public/js/main.js
@@ -1,48 +1,51 @@
 import Camera from './Camera.js';
+import Entity from './Entity.js';
+import PlayerController from './traits/PlayerController.js'
 import Timer from './Timer.js';
 import {createCollisionLayer, createCameraLayer} from './layers.js';
-import {loadLevel} from './loaders/level.js';
+import {createLevelLoader} from './loaders/level.js';
 import {loadEntities} from './entities.js';
 import {setupKeyboard} from './input.js';
 
-const canvas = document.getElementById('screen');
-const context = canvas.getContext("2d");
+function createPlayerEnv(playerEntity) {
+    const playerEnv = new Entity();
+    const playerControl = new PlayerController();
+    playerControl.checkpoint.set(64, 64);
+    playerControl.setPlayer(playerEntity);
+    playerEnv.addTrait(playerControl)
+    return playerEnv;
+}
 
-Promise.all([
-    loadEntities(),
-    loadLevel("1-1")
-])
-.then(([entity, level]) => {
+async function main(canvas) {
+    const context = canvas.getContext("2d");
+    const entityFactory = await loadEntities();
+    const loadLevel = await createLevelLoader(entityFactory);
+    
+    const level = await loadLevel("1-1");
+    
     const camera = new Camera();
     window.camera = camera;
+    
+    const mario = entityFactory.mario();
 
-    const mario = entity.mario();
-
-    mario.pos.set(64, 180);
-
-    const goomba = entity.goomba();
-    goomba.pos.x = 220;
-    level.entities.add(goomba);
-
-    const koopa = entity.koopa();
-    koopa.pos.x = 260;
-    level.entities.add(koopa);
-
-    level.entities.add(mario);
-
+    const playerEnv = createPlayerEnv(mario);
+    level.entities.add(playerEnv);
+    
     level.comp.layers.push(createCollisionLayer(level));
     
     const input = setupKeyboard(mario);
     input.listenTo(window);
-
+    
     const timer = new Timer();
     timer.update = function update(time) {
         level.update(time);
-        if (mario.pos.x > 100) {
-            camera.pos.x = mario.pos.x - 100;
-        }
+        camera.pos.x = Math.max(0, mario.pos.x - 100);
         level.comp.draw(context, camera);
     }
-
+    
     timer.start();
-})
+}
+
+const canvas = document.getElementById('screen');
+
+main(canvas);
